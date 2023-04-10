fd9ab60ffc0e547cc62d7c339c48a78044802254
alfredop89 <offsound2020@gmail.com>
2023-04-10 18:24:47 -0400

    first commit

---
+++ JS/main.js
@@ -0,0 +1,65 @@
+let body = document.querySelector('body')
+let header = document.querySelector('header')
+let menuBar = document.getElementById('menuBar')
+let dropMenu = document.getElementById('dropMenu')
+let contactoLink = document.querySelectorAll('.contacto-link')
+let contactoDiv = document.getElementById('contactoDiv')
+let fadeOutContainers = document.querySelectorAll('.fade-out')
+let lightMode = document.getElementById('lightMode')
+
+
+// BODY CLICK
+
+body.onclick = () => {
+    dropMenu.classList.remove('open-drop-menu')
+    contactoDiv.style.transform = 'translateX(1000px)'
+}
+// HEADER & MENU
+
+menuBar.onclick = (e) => {
+    e.stopPropagation()
+    dropMenu.classList.toggle('open-drop-menu')
+}
+dropMenu.onclick = () => {
+    dropMenu.classList.remove('open-drop-menu')
+}
+contactoLink.forEach((e) => {
+    e.onclick = (ev) => {
+        ev.stopPropagation()
+        ev.preventDefault()
+        contactoDiv.style.transform = 'translateX(0)'
+    }
+})
+contactoDiv.onclick = () => {
+    contactoDiv.style.transform = 'translateX(1000px)'
+}
+
+// SQUARE ANIMATION
+
+function squareAnimation() {
+
+    let div = document.createElement('div')
+    div.classList.add('square-animation')
+    body.appendChild(div)
+}
+squareAnimation()
+
+
+// WINDOW SCROLLING
+
+    window.onscroll = () => {
+
+        fadeOutContainers.forEach((e) => {
+
+            let windowHeight = window.innerHeight / 1.25
+            let topElement = e.getBoundingClientRect().top
+
+            topElement <= windowHeight ? e.classList.add('fade-in') : e.classList.remove('fade-in')
+        })
+    }
+
+// LIGHT MODE
+
+lightMode.onclick = () => {
+    body.classList.toggle('light')
+}
